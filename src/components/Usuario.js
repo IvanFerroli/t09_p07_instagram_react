3771c6abc726a642e5689d3e9f6f2399c5cc4bff
IvanFerroli <ivanilson.ferreira.mec@gmail.com>
2022-11-20 20:09:15 -0300

    feat: change image

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Usuario() {
 	const [nome, setNome] = React.useState("CatanaComics")
+	const [imagem, setImagem] = React.useState("./assets/img/catanacomics.svg")
 
 	return (
 		<div className="usuario">
-			<img alt="CatanaComics" src="./assets/img/catanacomics.svg" />
+			<img onClick={() => setImagem(prompt("Para qual imagem deseja alterar?"))} alt="CatanaComics" src={imagem} />
 			<div className="texto">
 				<strong>{nome}</strong>
 				<span>
