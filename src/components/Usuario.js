d4cc3088d106279fd6a63f460fdb85ee3038799f
IvanFerroli <ivanilson.ferreira.mec@gmail.com>
2022-11-21 03:19:55 -0300

    feat: edit name and pic fixes 3nd try

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,33 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function Usuario() {
-	const [nome, setNome] = React.useState("CatanaComics")
-	const [imagem, setImagem] = React.useState("./assets/img/catanacomics.svg")
+	const [nome, setNome] = React.useState("CatanaComics");
+	const [imagem, setImagem] = React.useState("./assets/img/catanacomics.svg");
 
 	function trocaNome() {
-		const nomeLindo = prompt("Para qual nome deseja alterar?")
-		if(nomeLindo === "" || nomeLindo === null){
-			trocaNome()
+		const nomeLindo = prompt("Para qual nome deseja alterar?");
+		if (nomeLindo === "" || nomeLindo === null) {
+			trocaNome();
+		} else {
+			return nomeLindo;
 		}
-		return nomeLindo
 	}
 
 	function trocaImagem() {
-		const imagemLinda = prompt("Para qual imagem deseja alterar?")
-		if(imagemLinda === "" || imagemLinda === null){
-			trocaImagem()
+		const imagemLinda = prompt("Para qual imagem deseja alterar?");
+		if (imagemLinda === "" || imagemLinda === null) {
+			trocaImagem();
+		} else {
+			return imagemLinda;
 		}
-		return imagemLinda
 	}
 
 	return (
 		<div className="usuario" data-test="user">
-			<img onClick={() => setImagem(trocaImagem)} alt="CatanaComics" src={imagem} data-test="profile-image"/>
+			<img
+				onClick={() => setImagem(trocaImagem)}
+				alt="CatanaComics"
+				src={imagem}
+				data-test="profile-image"
+			/>
 			<div className="texto">
 				<strong>{nome}</strong>
 				<span>
 					<span data-test="name">{nome}</span>
-					<ion-icon onClick={() => setNome(trocaNome)} name="pencil" data-test="edit-name"></ion-icon>
+					<ion-icon
+						onClick={() => setNome(trocaNome)}
+						name="pencil"
+						data-test="edit-name"
+					></ion-icon>
 				</span>
 			</div>
 		</div>
